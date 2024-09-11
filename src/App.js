985552f66319c414bfb5676826e9d7d58efe4a73
brandiw <brandimichelle.0911@gmail.com>
2024-09-11 11:17:12 -0700

    Created React App

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="App-banner-container">
+          <img src={'/rolls.jpg'} className="App-banner" alt="banner" />
+        </div>
+        <p>
+          Butler Family Recipes
+        </p>
+      </header>
+      <body className="App-body">
+        <p>Hi</p>
+      </body>
+      <footer className="App-footer">
+        <p>le foot</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
